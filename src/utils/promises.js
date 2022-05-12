936977fbd43200f9982189a1d527a8ad9bd891a1
lachero <asaf@utopia.tech>
2022-05-12 23:22:01 +0300

    removed file://

--- src/utils/promises.js
+++ src/utils/promises.js
@@ -1,5 +1,5 @@
 /** @param {Array} files  */
 export const importFiles = files => {
-	const promises = files.map(file => import(`file://${file}`));
+	const promises = files.map(file => import(file));
 	return Promise.all(promises);
 };
